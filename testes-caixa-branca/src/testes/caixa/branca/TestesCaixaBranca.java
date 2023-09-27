ed4205f3708f4bbeda7a4a22fb161f4a1c662dc5
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2023-09-27 18:33:05 -0300

    Add files via upload

---
+++ testes-caixa-branca/src/testes/caixa/branca/TestesCaixaBranca.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testes.caixa.branca;
+
+/**
+ *
+ * @author guima
+ */
+public class TestesCaixaBranca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
